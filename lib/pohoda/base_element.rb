af983db13f54debb08622b53075903491d049857
Premysl Donat <pdonat@seznam.cz>
2017-11-19 11:03:26 +0100

    Use nokogiri internal API to save some memory

--- lib/pohoda/base_element.rb
+++ lib/pohoda/base_element.rb
@@ -9,15 +9,39 @@ module Pohoda
     private
 
     def text_at(xpath)
-      xml.xpath(xpath)&.text
+      ctx = Nokogiri::XML::XPathContext.new(xml)
+      ctx.evaluate(xpath, nil)&.first&.text
     end
 
-    def at_xpath(xpath)
-      xml.at_xpath(xpath)
+    def at_xpath(locator)
+      result =
+        if xml.is_a? Nokogiri::XML::NodeSet
+          xml.inject(Nokogiri::XML::NodeSet.new(xml.document)) do |set, node|
+            ctx = Nokogiri::XML::XPathContext.new(node)
+            res = ctx.evaluate(locator, nil)
+
+            set + res
+          end
+        else
+          ctx = Nokogiri::XML::XPathContext.new(xml)
+          ctx.evaluate(locator, nil)
+        end
+
+      result&.first
     end
 
-    def xpath(xpath)
-      xml.xpath(xpath)
+    def xpath(locator)
+      if xml.is_a? Nokogiri::XML::NodeSet
+        xml.inject(Nokogiri::XML::NodeSet.new(xml.document)) do |set, node|
+          ctx = Nokogiri::XML::XPathContext.new(node)
+          res = ctx.evaluate(locator, nil)
+
+          set + res
+        end
+      else
+        ctx = Nokogiri::XML::XPathContext.new(xml)
+        ctx.evaluate(locator, nil)
+      end
     end
   end
 end
